67687abad30c6a14d584b0621887d112a32c85a9
karthika24-24 <karthika04119@gmail.com>
2025-09-01 10:12:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Menu for Mobile
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
